606bbd23c4561463242142445e9635bbb89169bc
risson <marcschmitt2@gmail.com>
2019-02-02 01:27:50 -0800

    level: refractor the way we handle blocks

--- level.py
+++ level.py
@@ -3,14 +3,37 @@ import pygame
 from physics import Space, Pos
 from constants import LEVELS_GRAPHICAL_FOLDER, MAPS_FOLDER
 
-# TODO: make dis a dictionnary
-START = ("P", None)
-EMPTY = (".", None)
-# TODO: choose randomly between available textures
-# TODO: remove grass and automatically add grass when there is no block over it
-GRASS = ("G", pygame.image.load(os.path.join(LEVELS_GRAPHICAL_FOLDER, "grass_000.png")))
-DIRT = ("D", pygame.image.load(os.path.join(LEVELS_GRAPHICAL_FOLDER, "dirt_000.png")))
-STONE = ("S", pygame.image.load(os.path.join(LEVELS_GRAPHICAL_FOLDER, "stone_000.png")))
+
+class Block:
+    BLOCKS = {
+        "P": (None, False, True),
+        ".": (None, False, False),
+        # TODO: choose randomly between available textures
+        # TODO: remove grass and automatically add grass when there is no block over it
+        "G": (pygame.image.load(os.path.join(LEVELS_GRAPHICAL_FOLDER, "grass_000.png")).convert(), True, False),
+        "D": (pygame.image.load(os.path.join(LEVELS_GRAPHICAL_FOLDER, "dirt_000.png")).convert(), True, False),
+        "S": (pygame.image.load(os.path.join(LEVELS_GRAPHICAL_FOLDER, "stone_000.png")).convert(), True, False),
+    }
+
+    def __init__(self, character='.'):
+        if not character in Block.BLOCKS:
+            raise Exception("Shit fuck")  # TODO: maybe be just an empty one
+        self.block = Block.BLOCKS[character]
+        self.img = self.block[0]
+        self.solid = self.block[1]
+        self.start = self.block[2]
+
+    def render(self, surf, pos=None):
+        if self.is_solid:
+            surf.blit(self.img, pos)
+
+    @property
+    def is_solid(self):
+        return self.solid
+
+    @property
+    def is_start(self):
+        return self.start
 
 
 class Level:
@@ -18,8 +41,6 @@ class Level:
         self.name = level
         self.space = Space(gravity=(0, 1))
         self.grid = []
-        self.height = 0
-        self.width = 0
         self.start = (0, 0)
         self.load_level()
 
@@ -29,34 +50,18 @@ class Level:
     def world_to_map(self, world_pos):
         return Pos(world_pos) // 16
 
-    def load_level(self):
+    def load_level(self):  # TODO: may we improve this?
         with open(os.path.join(MAPS_FOLDER, self.name + ".map"), 'r') as map_file:
             height, width = list(map(int, map_file.readline().split()))
-            self.height = height
-            self.width = width
             for h in range(height):
                 line = list(map_file.readline().strip())
                 for i in range(len(line)):
-                    if line[i] == "P":
-                        line[i] = START
+                    line[i] = Block(line[i])
+                    if line[i].is_start:
                         self.start = (h, i)
-                    elif line[i] == ".":
-                        line[i] = EMPTY
-                    elif line[i] == "G":
-                        line[i] = GRASS
-                    elif line[i] == "D":
-                        line[i] = DIRT
-                    elif line[i] == "S":
-                        line[i] = STONE
-                    else:
-                        print("yes")
-                        print(line[i])
-                        print("pouet")
-                        raise Exception("Shit fuck")
                 self.grid.append(line)
 
     def render(self, surf):
         for line in range(len(self.grid)):
             for block in range(len(self.grid[line])):
-                if self.grid[line][block] != START and self.grid[line][block] != EMPTY:
-                    surf.blit(self.grid[line][block][1], self.map_to_world((block, line)))
+                self.grid[line][block].render(surf, self.map_to_world((block, line)))
