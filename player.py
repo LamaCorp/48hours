d58edd4f470e5d3feea22042e2142f7c59917665
ddorn <diego.dorn@free.fr>
2019-02-02 09:14:52 +0100

    player: fixed lama size

--- player.py
+++ player.py
@@ -4,12 +4,11 @@ from physics import Body, AABB
 
 class Player(Body):
     def __init__(self, start_pos=(0, 0)):
-        shape = AABB(start_pos, (35, 32))
+        shape = AABB(start_pos, (38, 35))
         super().__init__(shape)
 
         self.img = pygame.image.load('assets/player/lama.png').convert()
-        self.img = pygame.transform.scale(self.img, (35, 32))
-        self.img.set_colorkey((255, 0, 255))
+        self.img.set_colorkey((255, 255, 255))
 
     def render(self, surf, offset=(0, 0)):
         surf.blit(self.img, self.topleft + offset)
