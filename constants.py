7f9ec85b2240519f594b219c21a4ba8cb1dc16a9
risson <marcschmitt2@gmail.com>
2019-02-02 01:04:09 -0800

    level: display map

--- constants.py
+++ constants.py
@@ -1,3 +1,5 @@
+import os
+
 # Screens
 MENU = 0
 GAME = 42
@@ -7,3 +9,10 @@ PICKER = 93933
 # Colors
 DARK = (31, 32, 65)
 LIGHT_DARK = (62, 64, 130)
+
+# Folder
+ASSETS = os.path.abspath(os.path.join(os.path.dirname(__file__), 'assets'))
+PLAYER_FOLDER = os.path.join(ASSETS, 'player')
+LEVELS_FOLDER = os.path.join(ASSETS, "levels")
+LEVELS_GRAPHICAL_FOLDER = os.path.join(LEVELS_FOLDER, "graphical")
+MAPS_FOLDER = os.path.join(LEVELS_FOLDER, "maps")
--- level.py
+++ level.py
@@ -1,15 +1,67 @@
+import os
+import pygame
 from physics import Space
+from constants import LEVELS_GRAPHICAL_FOLDER, MAPS_FOLDER
 
-EMPTY = (0, "O", None)
 
+DEFAULT_PATH = ""
+
+EMPTY = (0, "0")
+GRASS = (1, "G")
+DIRT = (2, "D")
+STONE = (3, "S")
+
+DEFAULT_PATH = ""
+
+# TODO: make dis a dictionnary
+START = ("P", None)
+EMPTY = (".", None)
+# TODO: choose randomly between available textures
+# TODO: remove grass and automatically add grass when there is no block over it
+GRASS = ("G", pygame.image.load(os.path.join(LEVELS_GRAPHICAL_FOLDER, "grass_000.png")))
+DIRT = ("D", pygame.image.load(os.path.join(LEVELS_GRAPHICAL_FOLDER, "dirt_000.png")))
+STONE = ("S", pygame.image.load(os.path.join(LEVELS_GRAPHICAL_FOLDER, "stone_000.png")))
 
 
 class Level:
     def __init__(self, level='level_0'):
         self.name = level
         self.space = Space(gravity=(0, 1))
-        # TODO: load it
+        self.grid = []
+        self.height = 0
+        self.width = 0
+        self.start = (0, 0)
+        self.load_level()
+
+    def load_level(self):
+        with open(os.path.join(MAPS_FOLDER, self.name + ".map"), 'r') as map_file:
+            height, width = list(map(int, map_file.readline().split()))
+            self.height = height
+            self.width = width
+            for h in range(height):
+                line = list(map_file.readline().strip())
+                for i in range(len(line)):
+                    if line[i] == "P":
+                        line[i] = START
+                        self.start = (h, i)
+                    elif line[i] == ".":
+                        line[i] = EMPTY
+                    elif line[i] == "G":
+                        line[i] = GRASS
+                    elif line[i] == "D":
+                        line[i] = DIRT
+                    elif line[i] == "S":
+                        line[i] = STONE
+                    else:
+                        print("yes")
+                        print(line[i])
+                        print("pouet")
+                        raise Exception("Shit fuck")
+                self.grid.append(line)
 
     def render(self, surf):
-        pass
-        # TODO: draw the map
+        size = surf.get_size()
+        for line in range(len(self.grid)):
+            for block in range(len(self.grid[line])):
+                if self.grid[line][block] != START and self.grid[line][block] != EMPTY:
+                    surf.blit(self.grid[line][block][1], (block * 17, line * 17))
