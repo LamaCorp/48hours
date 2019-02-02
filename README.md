5f000048a56667f66ba997306d3021beffdde261
risson <marcschmitt2@gmail.com>
2019-02-01 23:00:00 -0800

    Initial commit: start of 48 hours of crazyness

---
+++ README.md
@@ -0,0 +1,4 @@
+# 48 hours of code between two friends and a wonderful library
+---
+
+Let's see what can come out of it
---
+++ __main__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from graphalama.app import App
+import pygame
+pygame.init()
+
+
+def main():
+    App({}, None).run()
+
+
+if __name__ == "__main__":
+    main()
