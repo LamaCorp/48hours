a237fe3bed16373539411be9b471ba9919ceff3f
risson <marcschmitt2@gmail.com>
2019-02-03 05:23:14 -0800

    maps: add map 4 (which actually is map 2)

--- config.py
+++ config.py
@@ -36,8 +36,9 @@ PLAYERS = {
 LEVELS = {
     0: ("level_0.map", "0: EZ PZ"),
     1: ("level_1.map", "1: Can we get started?"),
-    2: ("level_2.map", "2: Get your shit together, Garry"),
-    3: ("level_3.map", "3: Getting to know each other"),
+    4: ("level_2.map", "2: Here we go"),
+    2: ("level_3.map", "3: Get your shit together, Garry"),
+    3: ("level_4.map", "4: Getting to know each other"),
 }
 
 get_available_levels = [LEVELS[l][1] for l in LEVELS]
