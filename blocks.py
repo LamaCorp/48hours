a5cbb60d61507d193626b87333037c1b534cb4db
risson <marcschmitt2@gmail.com>
2019-02-03 20:43:13 -0800

    bush: add bush

--- blocks.py
+++ blocks.py
@@ -59,6 +59,7 @@ class Block:
         dic = {
             "D": Dirt,
             "S": Stone,
+            "H": Bush,
             "C": CatCloud,
             "L": Lava,
             "B": Barbecue,
@@ -147,6 +148,22 @@ class Stone(Block):
         [".?. ??. .", ".?.???. .", ".?.?? . .", ".?. ? . ."],  # ". . ??.? ", ". .??  ?."],
         [". . ??. .", ". .???. .", ". .?? . .", ". . ? . ."],  # ".?  ??. .", " ?.?? . ."]
     ])
+
+
+class Bush(Block):
+    character = "H"
+    solid = False
+    visible = True
+    deadly = False
+
+    @classproperty
+    def sheet(cls):
+        if cls._sheet is None:
+            sheet = pygame.image.load(os.path.join(LEVELS_GRAPHICAL_FOLDER, "bush.png")).convert()
+            sheet.set_colorkey((255, 0, 255))
+            sheet = pygame.transform.scale(sheet, (DEFAULT_BLOCK_SIZE, DEFAULT_BLOCK_SIZE))
+            cls._sheet = sheet
+        return cls._sheet
 
 
 class CatCloud(Block):
@@ -263,6 +280,7 @@ BLOCKS = [
     Dirt,
     Stone,
     CatCloud,
+    Bush,
     Lava,
     Barbecue,
     FieryBarbecue,
